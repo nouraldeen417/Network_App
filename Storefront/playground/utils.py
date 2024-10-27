87bad72502046b4d444feeaf5afeeb09e061eb9b
Hagag <hishamhagag18@gmail.com>
2024-10-27 03:18:17 +0300

    commit

--- Storefront/playground/utils.py
+++ Storefront/playground/utils.py
@@ -1,21 +1,29 @@
 # utils.py
+import sys
+# Add the path to the 'python' folder to the system path
+sys.path.append("..")
+# Now import 'some_file' from the 'python' directory
+from python import hello
 class AutomationMethods:
     @staticmethod
-    def Ping():
-        # Here you can add your processing logic
-        devices = []
-        
-        # First device
-        d1 = Device(host="device1", task="task1", status="Running")
-        devices.append(d1)
-        
-        # Second device
-        d2 = Device(host="device2", task="task2", status="Stopped")
-        devices.append(d2)
-        return devices
     
-class Device:
-    def __init__(self, host, task, status):
-        self.host = host
-        self.task = task
-        self.status = status
+    def Ping():
+        status=hello.Ping() #status has three lists host-ip list[], status list[] ,task name list[]
+        print(status)
+        return  status
+# MY Data I used to Test 
+# class AutomationMethods:
+#     @staticmethod
+#     def Ping():
+#         # Here you can add your processing logic
+#         devices = []
+        
+#         # First device
+#         d1 = Device(host="device1", task="task1", status="Running")
+#         devices.append(d1)
+        
+#         # Second device
+#         d2 = Device(host="device2", task="task2", status="Stopped")
+#         devices.append(d2)
+#         return devices
+    
