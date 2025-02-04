a31a5e6f03b4561cae37657bf87b7390c5d00c41
nour <nouraldeennabil259@gmail.com>
2025-02-05 01:03:04 +0200

    modify ansible to support interface input

--- Storefront/playground/utils.py
+++ Storefront/playground/utils.py
@@ -3,28 +3,28 @@ import sys
 # Add the path to the 'python' folder to the system path
 sys.path.append("..")
 # Now import 'some_file' from the 'python' directory
-#from python import hello,router,configration
+from python import hello,router,configration
 class AutomationMethods:
     @staticmethod
     
     def Ping():
-        status=AutomationMethodsData.Ping() #status has three lists host-ip list[], status list[] ,task name list[]
+        status=hello.Ping() #status has three lists host-ip list[], status list[] ,task name list[]
         print(status)
         return  status
 
     def Router_list():
-        Fact_data=AutomationMethodsData.Routers_facts() #Fact_data has three lists host-ip list[], status list[] ,task name list[]
+        Fact_data=router.Routers_facts() #Fact_data has three lists host-ip list[], status list[] ,task name list[]
         
         print(Fact_data)
         return  Fact_data
     
     def Set_Hostname(router,hostname):
-        status = "successful"#configration.set_hostname(router,hostname)
+        status = configration.set_hostname(router,hostname)#"successful"
         print (status)
         return status
     
     def Set_Banner(router,banner):
-        status = "successful"#configration.set_banner(router,banner)
+        status = configration.set_banner(router,banner)#"successful"
         return status
     
     def set_interfaceconfigration(selected_host,interface_name,ipv4):
@@ -32,7 +32,7 @@ class AutomationMethods:
         print(selected_host)
         print(interface_name)
         print(ipv4)
-        status = "successful"
+        status = configration.set_interfaceconfigration(selected_host,interface_name,ipv4)#"successful"
         return status
     
     def Switch_list():
